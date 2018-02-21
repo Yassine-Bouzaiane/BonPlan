eed5c7cd26c6c63c9d45ee8770a5b2ac982d4985
ami123k <35659879+ami123k@users.noreply.github.com>
2018-02-21 22:49:55 +0100

    first commit
    :D

--- src/Services/ServiceEvenement.java
+++ src/Services/ServiceEvenement.java
@@ -6,7 +6,9 @@
 package Services;
 
 import Config.Connexion;
+import Entite.Etablissement;
 import Entite.Evenement;
+import Entite.Offre;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,7 +19,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import IServices.IServiceEvenement;
 import java.sql.Connection;
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import jdk.nashorn.internal.runtime.regexp.joni.EncodingHelper;
 
 /**
@@ -41,11 +46,19 @@ public Connection cnx;
             while(rest.next()){
                 Evenement ev = new Evenement();
                 ev.setId_evenement(rest.getInt("id_evenement"));
-                ev.setDescription_evenement(rest.getString("description_evenement"));
-                ev.setDate_evenement(rest.getDate("date_evenement"));
-                ev.setPhoto_evenement(rest.getString("photo_evenement"));
-                ev.getEtablissement().setId_etablissement(rest.getInt("id_etablissement"));
+                ev.setDescription(rest.getString("description_evenement"));
+               Timestamp input =rest.getTimestamp("date_evenement");   
+         LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+         LocalDateTime newD = rest.getTimestamp("date_evenement").toLocalDateTime();
+         java.util.Date ddd = new java.util.Date();
+         ev.setDate_evenement(date);
+         
+                ev.setPhoto(rest.getString("photo_evenement"));
+                ev.getEtab().setId_etablissement(rest.getInt("id_etablissement"));
                 ev.setNom_evenement(rest.getString("nom_evenement"));
+                ev.setNombre_interesse(rest.getInt("Nombre_interesse"));
+                ev.setNombre_participant(rest.getInt("Nombre_participant"));
+                
                 evs.add(ev);
                 
             }
@@ -55,6 +68,42 @@ public Connection cnx;
         }
          return evs;
     }
+    public List<Evenement> afficherevnementbyid(int id) {
+    
+    {
+                List<Evenement> evenements =new ArrayList<>();
+        try {
+            Statement stm = Connexion.getInstance().getCon().createStatement();
+            ResultSet rest= 
+                    stm.executeQuery("select * from `evenement` where id_etablissement="+id);
+            while(rest.next()){
+                Evenement eve = new Evenement();
+                 eve.setId_evenement(rest.getInt("id_evenement"));
+                eve.setDescription(rest.getString("description_evenement"));
+                Timestamp input =rest.getTimestamp("date_evenement");   
+         LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+         LocalDateTime newD = rest.getTimestamp("date_evenement").toLocalDateTime();
+         java.util.Date ddd = new java.util.Date();
+         eve.setDate_evenement(date);
+         
+                eve.setPhoto(rest.getString("photo_evenement"));
+                eve.getEtab().setId_etablissement(rest.getInt("id_etablissement"));
+                eve.setNom_evenement(rest.getString("nom_evenement"));
+                evenements.add(eve);
+               
+ eve.setEtab(new ServiceEtablissement().chercherEtablissement(rest.getInt(8)));
+                evenements.add(eve);
+                
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ServiceOffre.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         return evenements;
+    }
+
+    }
+    
 
      public List<Evenement> afficherevenementbynom(String nom) 
     {
@@ -66,10 +115,15 @@ public Connection cnx;
             while(rest.next()){
                 Evenement ev = new Evenement();
                 ev.setId_evenement(rest.getInt("id_evenement"));
-                ev.setDescription_evenement(rest.getString("description_evenement"));
-                ev.setDate_evenement(rest.getDate("date_evenement"));
-                ev.setPhoto_evenement(rest.getString("photo_evenement"));
-                ev.getEtablissement().setId_etablissement(rest.getInt("id_etablissement"));
+                ev.setDescription(rest.getString("description_evenement"));
+               Timestamp input =rest.getTimestamp("date_evenement");   
+         LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+         LocalDateTime newD = rest.getTimestamp("date_evenement").toLocalDateTime();
+         java.util.Date ddd = new java.util.Date();
+         ev.setDate_evenement(date);
+         
+                ev.setPhoto(rest.getString("photo_evenement"));
+                ev.getEtab().setId_etablissement(rest.getInt("id_etablissement"));
                 ev.setNom_evenement(rest.getString("nom_evenement"));
                 evs.add(ev);
                 
@@ -90,10 +144,15 @@ public Connection cnx;
             while(rest.next()){
                 Evenement ev = new Evenement();
                 ev.setId_evenement(rest.getInt("id_evenement"));
-                ev.setDescription_evenement(rest.getString("description_evenement"));
-                ev.setDate_evenement(rest.getDate("date_evenement"));
-                ev.setPhoto_evenement(rest.getString("photo_evenement"));
-                ev.getEtablissement().setId_etablissement(rest.getInt("id_etablissement"));
+                ev.setDescription(rest.getString("description_evenement"));
+               Timestamp input =rest.getTimestamp("date_evenement");   
+         LocalDate date1 = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+         LocalDateTime newD = rest.getTimestamp("date_evenement").toLocalDateTime();
+         java.util.Date ddd = new java.util.Date();
+         ev.setDate_evenement(date1);
+         
+                ev.setPhoto(rest.getString("photo_evenement"));
+                ev.getEtab().setId_etablissement(rest.getInt("id_etablissement"));
                 ev.setNom_evenement(rest.getString("nom_evenement"));
                 evs.add(ev);
                 
@@ -107,34 +166,35 @@ public Connection cnx;
 
     @Override
     public void modifierevenement(Evenement e) {
-try {
-            String query = 
-                    
-         "update `evenement` set  description_evenement =?, date_evenement=?, photo_evenement=? where id_etablissement =?  ;";
-            
-            PreparedStatement st = Connexion.getInstance().getCon().prepareStatement(query);
 
-            st.setString(1,e.getDescription_evenement());
-            st.setDate(2,e.getDate_evenement());
-            st.setString(3,e.getPhoto_evenement());
-            st.setInt(4,4/*e.getEtablissement().getId_etablissement()*/);
-
-            st.executeUpdate();
-            System.out.println(e.getId_evenement());
-
-        } catch (SQLException ex) {
-            Logger.getLogger(ServiceEvenement.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
+ String sql ="UPDATE evenement SET nom_evenement = '"+e.getNom_evenement()+"',description_evenement = '"
+                    +e.getDescription()+"',date_evenement = '"+e.getDate_evenement()+"', photo_evenement = '"
+                    +e.getPhoto()+"' WHERE id_evenement ="+ e.getId_evenement()+";";
+        try 
+        {
+            Statement stl = cnx.createStatement();
+            stl.executeUpdate(sql);
+            System.out.println("Update Evenement done");
+        } 
+        catch (SQLException ex) 
+        {
+            System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        }        }
+    
     @Override
     public void supprimerevenement(Evenement e) {
- try {
-            String query = "delete from `evenement` where id_evenement =?";
-            PreparedStatement st = Connexion.getInstance().getCon().prepareStatement(query);
-            st.setInt(1,5/* e.getEtablissement().getId_etablissement()*/);
-            st.executeUpdate();
-            System.out.println("supp even ok");
+ try { String delete = "DELETE FROM evenement WHERE id_evenement = ? ";
+        PreparedStatement st2 = cnx.prepareStatement(delete);
+        int id = e.getId_evenement();
+        
+        st2.setInt(1,id);
+ 
+
+
+        st2.executeUpdate();
+       
 
         } catch (SQLException ex) {
             Logger.getLogger(ServiceEvenement.class.getName()).log(Level.SEVERE, null, ex);
@@ -144,19 +204,12 @@ try {
     @Override
     public void ajouterevenement(Evenement e) {
 try {
-            String query = "insert into `evenement` (`description_evenement`,`date_evenement`,`photo_evenement`,`id_etablissement`,`nom_evenement`) values (?,?,?,?,?)";
+            String query = "insert into `evenement` (`description_evenement`,`date_evenement`,`photo_evenement`,`id_etablissement`,`nom_evenement`) values ('"+e.getDescription()+"','"+e.getDate_evenement()+"','"+e.getPhoto()+"',"+e.getEtab().getId_etablissement()+",'"+e.getNom_evenement()+"')";
             PreparedStatement st = Connexion.getInstance().getCon().prepareStatement(query,PreparedStatement.RETURN_GENERATED_KEYS);
            
            
-            st.setString(1,e.getDescription_evenement());
-            st.setDate(2,e.getDate_evenement());
-            st.setString(3,e.getPhoto_evenement()); 
-           
-            st.setInt(4,e.getEtablissement().getId_etablissement());
-               st.setString(5,e.getNom_evenement());
             st.executeUpdate();
-          
-   ResultSet result = st.getGeneratedKeys();
+             ResultSet result = st.getGeneratedKeys();
             int id = 0;
             while (result.next()) {
                 id = result.getInt(1);
@@ -180,11 +233,15 @@ Evenement of= new  Evenement();
         ResultSet result = statement1.executeQuery(select);
        
         while (result.next()) 
-        {
-            of.setDate_evenement(result.getDate("date_evenement"));
-            of.setDescription_evenement(result.getString("description_evenement"));
+        {Timestamp input =result.getTimestamp("date_evenement");   
+         LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+         LocalDateTime newD = result.getTimestamp("date_evenement").toLocalDateTime();
+         java.util.Date ddd = new java.util.Date();
+         of.setDate_evenement(date);
+            
+            of.setDescription(result.getString("description_evenement"));
             of.setId_evenement(result.getInt("id_evenement"));
-                                of.setEtablissement(new ServiceEtablissement().chercherEtablissement(result.getInt(8)));
+                                of.setEtab(new ServiceEtablissement().chercherEtablissement(result.getInt(8)));
 
         }
         }
@@ -197,8 +254,122 @@ Evenement of= new  Evenement();
         return of;
     }
 
-    
+   public String afficherlangitude(Evenement a) 
+    {
+     String position = "";
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet rest= 
+                    stm.executeQuery("select `langitude` from evenement e , etablissement b where e.id_etablissement=b.id_etablissement  ");
+            while(rest.next()){
 
+                
+              position = rest.getString("langitude");
+             
+               
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ServiceEvenement.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+         return position;
+    }
+      public String afficherlatitude(Evenement a) 
+    {
+     String position = "";
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet rest= 
+                    stm.executeQuery("select `latitude` from evenement e , etablissement b where e.id_etablissement=b.id_etablissement  ");
+            while(rest.next()){
+
+                
+              position = rest.getString("latitude");
+             
+               
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ServiceEvenement.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+         return position;
+    }
+     
+  
+ int participer ;
+   public void participant(Evenement e) 
+
+     {
+        try 
+    {   
+        Statement stm = cnx.createStatement();
+    
+            ResultSet rest= 
+                    stm.executeQuery("SELECT nombre_participant FROM evenement where id_evenement = "+e.getId_evenement()+" ;");
+            while(rest.next()){
+
+                
+                
+              this.participer = rest.getInt("nombre_participant");
+                System.err.println(participer);}
+             this.participer = participer +1 ;
+             System.out.println(participer);
+     
+        String sql ="UPDATE evenement SET nombre_participant = "+participer +" where id_evenement = "+e.getId_evenement()+";";
+       
+//            System.out.println(e.getNombre_interesse());
+            Statement stl = cnx.createStatement();
+            stl.executeUpdate(sql);
+//            System.out.println(e.getId_evenement());
+            System.out.println(e.getNombre_participant());
+//            System.out.println("Update Evenement done");
+
+        } 
+        catch (SQLException ex) 
+        {
+            System.err.println("SQLExceptioninteressant: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        }        }
+
+                
+ 
+int interesser ;
+ public void interesse(Evenement e) 
+    {
+        try 
+    {   
+        Statement stm = cnx.createStatement();
+    
+            ResultSet rest= 
+                    stm.executeQuery("SELECT nombre_interesse FROM evenement where id_evenement = "+e.getId_evenement()+" ;");
+            while(rest.next()){
+
+                
+                
+              this.interesser = rest.getInt("nombre_interesse");
+                System.err.println(interesser);}
+             this.interesser = interesser +1 ;
+             System.out.println(interesser);
+     
+        String sql ="UPDATE evenement SET nombre_interesse = "+interesser +" where id_evenement = "+e.getId_evenement()+";";
+       
+//            System.out.println(e.getNombre_interesse());
+            Statement stl = cnx.createStatement();
+            stl.executeUpdate(sql);
+//            System.out.println(e.getId_evenement());
+            System.out.println(e.getNombre_interesse());
+//            System.out.println("Update Evenement done");
+
+        } 
+        catch (SQLException ex) 
+        {
+            System.err.println("SQLExceptioninteressant: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+        }        }
  }
 
  
